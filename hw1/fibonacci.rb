64c79e055c82ba6c6babdc73281b4137b5eb389f
Sergey Nanovsky <sergey.nanovsky@gmail.com>
2016-11-04 23:58:36 +0200

    Homework 1: Generate sequence during iteration

--- hw1/fibonacci.rb
+++ hw1/fibonacci.rb
@@ -1,38 +1,31 @@
 class Fibonacci
   include Enumerable
 
-  attr_reader :sequence
-
   def initialize(length)
     if valid_length?(length)
-      create_sequence(length)
+      @length = length
     else
       raise ArgumentError, "Sequence length isn't valid."
     end
   end
 
   def each
-    @sequence.each { |item| yield(item) }
+    previous = 1
+    current = 1
+    @length.times do |i|
+      if i < 2
+        yield 1
+      else
+        new_value = previous + current
+        previous, current = current, new_value
+        yield new_value
+      end
+    end
   end
 
   private
 
   def valid_length?(length)
     length.is_a?(Integer) && length > 0
-  end
-
-  def create_sequence(length)
-    @sequence = Array.new(length)
-    previous = 1
-    current = 1
-    length.times do |i|
-      if i < 2
-        @sequence[i] = 1
-      else
-        @sequence[i] = previous + current
-        previous = current
-        current = sequence[i]
-      end
-    end
   end
 end
