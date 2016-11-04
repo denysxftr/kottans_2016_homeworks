72f393107b193a08e7a82b8f61d06b4e820c8152
Denis Sergienko <olol.toor@gmail.com>
2016-11-04 13:02:39 +0200

    homework 1

---
+++ hw1/fibonacci_test.rb
@@ -0,0 +1,42 @@
+require './hw1/fibonacci'
+
+def assert(expect:, to_equal: true, text:)
+  if expect == to_equal
+    puts "\e[32m#{text}: OK!\e[0m"
+  else
+    puts "\e[31m#{text}: FAILED!\e[0m"
+    puts "Expected: #{expect.inspect}"
+    puts "To equal: #{to_equal.inspect}"
+    exit(1)
+  end
+end
+
+assert(
+  expect: defined?(Fibonacci),
+  to_equal: 'constant',
+  text: 'class defined'
+)
+
+sequence = Fibonacci.new(1)
+assert(
+  expect: sequence.methods.include?(:each),
+  text: 'has method each'
+)
+
+assert(
+  expect: Fibonacci.ancestors.include?(Enumerable),
+  text: 'has enumerable included'
+)
+
+assert(
+  expect: sequence.map(&:to_s),
+  to_equal: ["1"],
+  text: 'map works for one element'
+)
+
+sequence = Fibonacci.new(10)
+assert(
+  expect: sequence.map(&:to_s),
+  to_equal: ["1", "1", "2", "3", "5", "8", "13", "21", "34", "55"],
+  text: 'map works for many elements'
+)
