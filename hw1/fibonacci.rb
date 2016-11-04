96387fd1b00d6862f89bfdbf64ad6354c8052a7b
Igor Sviatniy <sviatniy@gmail.com>
2016-11-04 23:49:15 +0200

    Initial solution

---
+++ hw1/fibonacci.rb
@@ -0,0 +1,16 @@
+class Fibonacci
+  include Enumerable
+
+  def initialize(length)
+    @length = length
+  end
+
+  def each
+    fibs = [1]
+    loop do
+      yield fibs.last
+      break if fibs.length == @length
+      fibs << fibs.last(2).reduce(:+)
+    end
+  end
+end
