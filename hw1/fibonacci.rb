3457769a7546bce350889c324d5bda53f627b908
fastmanV <vitaliy.sinugin@gmail.com>
2016-11-06 09:55:03 +0200

    Update fibonacci.rb

---
+++ hw1/fibonacci.rb
@@ -0,0 +1,29 @@
+class Fibonacci
+ include Enumerable
+
+def fibonacci(n)
+    a = 1
+    b = 1
+    n.times do
+        temp = a
+        a = b
+        b = temp + b
+    end
+
+    return a
+end
+
+
+def each(&block)
+    @num.times do |n|
+    block.call(fibonacci(n))
+end
+
+
+  end
+
+def initialize(num)
+@num = num
+
+        end
+end
