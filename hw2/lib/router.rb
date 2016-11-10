332f582c575e495c9cf57b143fc302a206960eb0
Sergey Nanovsky <sergey.nanovsky@gmail.com>
2016-11-10 16:06:35 +0200

    Homework 2: Solution

--- hw2/lib/router.rb
+++ hw2/lib/router.rb
@@ -1,6 +1,6 @@
 class Router
   def call(env)
-    @routes[env['REQUEST_METHOD']][env['REQUEST_PATH']].call(env)
+    app_for_env(env).call(env)
   end
 
   private
@@ -18,8 +18,40 @@ class Router
     match('POST', path, rack_app)
   end
 
+  def patch(path, rack_app)
+    match('PATCH', path, rack_app)
+  end
+
+  def put(path, rack_app)
+    match('PUT', path, rack_app)
+  end
+
+  def delete(path, rack_app)
+    match('DELETE', path, rack_app)
+  end
+
+  def app_for_env(env)
+    app = nil
+    @routes[env['REQUEST_METHOD']].each do |path|
+      unless (path[:regexp] =~ env['REQUEST_PATH']).nil?
+        app = path[:app]
+        break
+      end
+    end
+    app = ->(_) { [404, {}, ['page not found']] } if app.nil?
+    app
+  end
+
+  def path_regexp(path)
+    parts = path.split('/').map do |part|
+      part = part.replace('[A-Za-z0-9_-]+') if part.include? ':'
+      part
+    end
+    Regexp.new('\A' + parts.join('\/') + '\z')
+  end
+
   def match(http_method, path, rack_app)
-    @routes[http_method] ||= {}
-    @routes[http_method][path] = rack_app
+    @routes[http_method] ||= []
+    @routes[http_method] << { regexp: path_regexp(path), app: rack_app }
   end
 end
--- hw2/spec/lib/router_spec.rb
+++ hw2/spec/lib/router_spec.rb
@@ -1,34 +1,77 @@
 RSpec.describe Router do
   subject do
     Router.new do
-      get '/test', ->(_) { [200, {}, ['get test']] }
-      post '/test', ->(_) { [200, {}, ['post test']] }
-
-      ##
-      # TODO: router should match path by pattern like
-      # Pattern: /posts/:name
-      # Paths:
-      # /post/about_ruby
-      # /post/43
-      # Cover this with tests.
-      #
-      get '/post/:name', ->(_) { [200, {}, ['post show page']] }
+      get '/posts', ->(_) { [200, {}, ['get posts']] }
+      get '/posts/:id', ->(_) { [200, {}, ['show post']] }
+      post '/posts', ->(_) { [200, {}, ['create post']] }
+      patch '/posts/:id', ->(_) { [200, {}, ['update post']] }
+      put '/posts/:id', ->(_) { [200, {}, ['update post']] }
+      delete '/posts/:id', ->(_) { [200, {}, ['delete post']] }
     end
   end
 
   context 'when request is GET' do
-    let(:env) { { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'GET' } }
+    it 'matches /posts request' do
+      env = { 'REQUEST_PATH' => '/posts', 'REQUEST_METHOD' => 'GET' }
+      expect(subject.call(env)).to eq [200, {}, ['get posts']]
+    end
 
-    it 'matches request' do
-      expect(subject.call(env)).to eq [200, {}, ['get test']]
+    it 'matches /posts/:id request' do
+      env = { 'REQUEST_PATH' => '/posts/123_a-b', 'REQUEST_METHOD' => 'GET' }
+      expect(subject.call(env)).to eq [200, {}, ['show post']]
+    end
+
+    it 'returns 404 if path not found' do
+      env = { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'GET' }
+      expect(subject.call(env)).to eq [404, {}, ['page not found']]
     end
   end
 
   context 'when request is POST' do
-    let(:env) { { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'POST' } }
+    it 'matches /posts request' do
+      env = { 'REQUEST_PATH' => '/posts', 'REQUEST_METHOD' => 'POST' }
+      expect(subject.call(env)).to eq [200, {}, ['create post']]
+    end
 
-    it 'matches request' do
-      expect(subject.call(env)).to eq [200, {}, ['post test']]
+    it 'returns 404 if path not found' do
+      env = { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'POST' }
+      expect(subject.call(env)).to eq [404, {}, ['page not found']]
+    end
+  end
+
+  context 'when request is PATCH' do
+    it 'matches /posts/:id request' do
+      env = { 'REQUEST_PATH' => '/posts/123_a-b', 'REQUEST_METHOD' => 'PATCH' }
+      expect(subject.call(env)).to eq [200, {}, ['update post']]
+    end
+
+    it 'returns 404 if path not found' do
+      env = { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'PATCH' }
+      expect(subject.call(env)).to eq [404, {}, ['page not found']]
+    end
+  end
+
+  context 'when request is PUT' do
+    it 'matches /posts/:id request' do
+      env = { 'REQUEST_PATH' => '/posts/123_a-b', 'REQUEST_METHOD' => 'PUT' }
+      expect(subject.call(env)).to eq [200, {}, ['update post']]
+    end
+
+    it 'returns 404 if path not found' do
+      env = { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'PUT' }
+      expect(subject.call(env)).to eq [404, {}, ['page not found']]
+    end
+  end
+
+  context 'when request is DELETE' do
+    it 'matches /posts/:id request' do
+      env = { 'REQUEST_PATH' => '/posts/123_a-b', 'REQUEST_METHOD' => 'DELETE' }
+      expect(subject.call(env)).to eq [200, {}, ['delete post']]
+    end
+
+    it 'returns 404 if path not found' do
+      env = { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'DELETE' }
+      expect(subject.call(env)).to eq [404, {}, ['page not found']]
     end
   end
 end
