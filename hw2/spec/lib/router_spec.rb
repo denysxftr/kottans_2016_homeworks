a884df7fba1252901f39597e75c896d756cca1f4
Andrii_Drozdov <drodjins@gmail.com>
2016-11-11 20:21:23 +0200

    Wrote all tests

--- hw2/spec/lib/router_spec.rb
+++ hw2/spec/lib/router_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Router do
       get '/test', ->(env) { [200, {}, ['get test']] }
       post '/test', ->(env) { [200, {}, ['post test']] }
 
+      get '/post/:name', ->(env) { [200, {}, []] }
+
       ##
       # TODO: router should match path by pattern like
       # Pattern: /posts/:name
@@ -12,7 +14,7 @@ RSpec.describe Router do
       # /post/43
       # Cover this with tests.
       #
-      
+
       get '/post/:name', ->(env) { [200, {}, ['post show page']] }
     end
   end
@@ -32,4 +34,25 @@ RSpec.describe Router do
       expect(subject.call(env)).to eq [200, {}, ['post test']]
     end
   end
+
+  context 'when visiting not existing page' do
+   let(:env) { {'REQUEST_PATH' => '/not_found_path', 'REQUEST_METHOD' => 'GET'} }
+   it '404 error must appear' do
+     expect(subject.(env)).to eq [404, {}, ['page not found']]
+   end
+ end
+
+ context 'when visiting /post' do
+   let(:env) { {'REQUEST_PATH' => '/post/43', 'REQUEST_METHOD' => 'GET'} }
+   it 'we should see /43 post' do
+     expect(subject.(env)).to eq [200, {}, ['post 43']]
+   end
+ end
+
+ context 'when visiting /post' do
+   let(:env) { {'REQUEST_PATH' => '/post/about_ruby', 'REQUEST_METHOD' => 'GET'} }
+   it 'we should see /about_ruby post' do
+     expect(subject.(env)).to eq [200, {}, ['post about_ruby']]
+   end
+ end
 end
