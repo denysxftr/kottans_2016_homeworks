25e588283d9c919656182664cda54b6f1e747dc9
Serg <yelagins@gmail.com>
2016-11-14 01:14:58 +0200

    router

--- hw2/app/app.rb
+++ hw2/app/app.rb
@@ -1,4 +1,7 @@
 Application = Router.new do
-  get '/test', ->(env) { [200, {}, ['get test']] }
-  post '/test', ->(env) { [200, {}, ['post test']] }
+  get '/test1',               ->(env) {[200, {}, ['Page 1']]}
+  get '/test2/test:id',       ->(env) {[200, {}, ['Page 2']]}
+  get '/test3/:id',           ->(env) {[200, {}, ['Page 3']]}
+  get '/test4/:id/test5',     ->(env) {[200, {}, ['Page 4']]}
+  get '/test4/:id/test5/:id', ->(env) {[200, {}, ['Page 5']]}
 end
--- hw2/spec/lib/router_spec.rb
+++ hw2/spec/lib/router_spec.rb
@@ -31,4 +31,28 @@ RSpec.describe Router do
       expect(subject.call(env)).to eq [200, {}, ['post test']]
     end
   end
+
+  context 'when request is GET & PATH is /post/about_ruby' do
+    let(:env) {{ 'REQUEST_PATH' => '/post/about_ruby', 'REQUEST_METHOD' => 'GET'}}
+
+    it 'matches request' do
+      expect(subject.call(env)).to eq [200, {}, ['post show page']]
+    end
+  end
+
+  context 'when request is GET & PATH is /post/43' do
+    let(:env) {{ 'REQUEST_PATH' => '/post/43', 'REQUEST_METHOD' => 'GET'}}
+
+    it 'matches request' do
+      expect(subject.call(env)).to eq [200, {}, ['post show page']]
+    end
+  end
+
+  context 'when page not found' do
+    let(:env) {{ 'REQUEST_PATH' => '/post/about_ruby/43', 'REQUEST_METHOD' => 'GET'}}
+
+    it 'matches request' do
+      expect(subject.call(env)).to eq [404, {}, ['404']]
+    end
+  end
 end
