93b919298a8e8ba2a08277658c7114971e0e0e8d
fastmanV <vitaliy.sinugin@gmail.com>
2016-11-13 22:33:09 +0200

    Update class.rb

--- hw2/class.rb
+++ hw2/class.rb
@@ -1,15 +1,1 @@
-class HelloWorld
-  def call(env)
-    req = Rack::Request.new(env)
-    case req.path_info
-    when /hello/
-      [200, {"Content-Type" => "text/html"}, ["Hello World!"]]
-    when /goodbye/  
-      [500, {"Content-Type" => "text/html"}, ["Goodbye Cruel World!"]]
-    else
-      [404, {"Content-Type" => "text/html"}, ["I'm Lost!"]]
-    end
-  end
-end
 
-run HelloWorld.new
