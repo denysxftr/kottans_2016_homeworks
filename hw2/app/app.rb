dae5050d03e30412a80587fe42d861833730c480
xwzsd <shamik2024@gmail.com>
2016-11-13 22:58:06 +0200

    hw2

--- hw2/app/app.rb
+++ hw2/app/app.rb
@@ -1,4 +1,7 @@
 Application = Router.new do
+  get '/', ->(env) { [200, {}, ['root path']] }
+  post '/', ->(env) { [200, {}, ['root path']] }
   get '/test', ->(env) { [200, {}, ['get test']] }
   post '/test', ->(env) { [200, {}, ['post test']] }
+  get '/blog/post/:id', ->(env) { [200, {}, ['post show page']] }
 end
--- hw2/lib/router.rb
+++ hw2/lib/router.rb
@@ -1,9 +1,13 @@
 class Router
   def call(env)
-    @routes[env['REQUEST_METHOD']][env['REQUEST_PATH']].call(env)
+      @routes[env['REQUEST_METHOD']].each do |v|
+      return v[:app].call(env) if env['REQUEST_PATH'] =~ v[:pattern]
+    end
+    [404, {}, ['Not found']]
   end
 
-private
+
+  private
 
   def initialize(&block)
     @routes = {}
@@ -19,7 +23,11 @@ private
   end
 
   def match(http_method, path, rack_app)
-    @routes[http_method] ||= {}
-    @routes[http_method][path] = rack_app
+    @routes[http_method] ||= []
+    @routes[http_method].push({ app: rack_app, pattern: to_regexp(path) })
+  end
+
+  def to_regexp(path)
+    Regexp.new('\A' + path.gsub(/:\w+/, '\w+') + '\Z')
   end
 end
