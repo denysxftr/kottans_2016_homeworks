0613d7118000ce1682002ed5136712468f061ca1
AlexPurhalo <alexpurhalo@gmail.com>
2016-11-14 00:57:33 +0200

    router-nested-params-and-404-implementation

--- hw2/app/app.rb
+++ hw2/app/app.rb
@@ -1,4 +1,15 @@
 Application = Router.new do
-  get '/test', ->(env) { [200, {}, ['get test']] }
-  post '/test', ->(env) { [200, {}, ['post test']] }
+  # posts
+  get     '/posts',                    ->(env) { [200, {}, ['shows the posts'   ]] }             # /posts
+  post    '/posts',                    ->(env) { [200, {}, ['creates a post'    ]] }             # /posts
+  get     '/posts/:name',              ->(env) { [200, {}, ['shows a post'      ]] }             # /posts/1
+  put     '/posts/:name',             ->(env) { [200, {}, ['updates a post'    ]] }             # /posts/1
+  delete  '/posts/:name',              ->(env) { [200, {}, ['destroys a post'   ]] }             # /posts/1
+
+  # comments that nested in post
+  get     '/posts/:name/comments',     ->(env) { [200, {}, ['shows the comments']] }              # /posts/1/comments
+  post    '/posts/:name/comments',    ->(env) { [200, {}, ['creates a comment' ]] }              # /posts/1/comments
+  get     '/posts/:name/comments/:id', ->(env) { [200, {}, ['shows a comment'   ]] }              # /posts/1/comments/1
+  put     '/posts/:name/comments/:id', ->(env) { [200, {}, ['updates a comment' ]] }              # /posts/1/comments/1
+  delete  '/posts/:name/comments/:id', ->(env) { [203, {}, ['destroys a comment']] }              # /posts/1/comments/1
 end
--- hw2/lib/router.rb
+++ hw2/lib/router.rb
@@ -1,25 +1,47 @@
 class Router
   def call(env)
-    @routes[env['REQUEST_METHOD']][env['REQUEST_PATH']].call(env)
+    req_path, req_method = env['REQUEST_PATH'], env['REQUEST_METHOD']  # storing request's method and path to variables
+    lambda_to_call = nil                              # default value setting to indicate incorrect requests for future
+    @routes.each do |route|                                   # iterating through routes array to get interesting route
+      # compares paths from route and request path with regular expressions
+      # route path in reg exp: posts/\w/comments/\w
+      # request path: posts/12/comments/2 will be accepted while verification by reg exp
+      lambda_to_call = route[:app] if route[:path_regexp].match(req_path) && route[:method] == req_method
+    end
+
+    # calls route's lambda if request's path passed verification by route's reg exp, otherwise shows not found
+    lambda_to_call.nil? ? [404, {}, ["Not found :("]] : lambda_to_call.call(env)
   end
 
-private
-
+  private
+  # receives routes lambdas and execute them
   def initialize(&block)
-    @routes = {}
+    @routes = []
+
     instance_exec(&block)
   end
 
-  def get(path, rack_app)
-    match('GET', path, rack_app)
+  # matches defined request methods
+  %w(get post put delete).each do |http_method|
+    define_method http_method do |path, rack_app|
+      match(http_method.upcase, path, rack_app) # sends: 'GET', '/posts', lambda
+    end
   end
 
-  def post(path, rack_app)
-    match('POST', path, rack_app)
+  # stocks methods in routes instance
+  def match(http_method, path, rack_app) # receives: 'GET', '/posts', lambda
+    new_obj = {
+        method: http_method,
+        path: path,
+        app: rack_app,
+        path_regexp: reg_exp_for_path(path)
+    }
+
+    @routes.push(new_obj)
   end
 
-  def match(http_method, path, rack_app)
-    @routes[http_method] ||= {}
-    @routes[http_method][path] = rack_app
-  end
+  # converts path from request to regular expression
+  def reg_exp_for_path(path)
+    Regexp.new('\A' + path.gsub(/:\w+/, '\w+') + '\Z')                      # changes /posts/:id to /posts/with-any-word
+  end                                                                                      # \w  is same to [a-zA-Z0-9_]
 end
