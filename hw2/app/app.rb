b2d7280abcf96741f185ad8731d2d9bc9c40bc12
Vladimir Gachkovsky <v.gachkovsky@gmail.com>
2016-11-14 13:11:48 +0200

    remove excess route

--- hw2/app/app.rb
+++ hw2/app/app.rb
@@ -2,8 +2,5 @@ Application = Router.new do
   get '/test', ->(env) { [200, {}, ['get test']] }
   post '/test', ->(env) { [200, {}, ['post test']] }
 
-  get '/sergey/:family_name', ->(env) { [200, {}, ["Hello Sergey #{env['params']['family_name']}"]] }
-
-  get '/post/:name/article/:id', ->(env) { [200, {}, ["get post #{env['params']['name']} and article #{env['params']['id']}"]] }
   get  '/post/:name', ->(env) { [200, {}, ["get post with name #{env['params']['name']}"]] }
 end
