9407c0979361066a70f2fc0456ecf9ea43690ab1
Dima Rymar <dimarymar@gmail.com>
2016-11-14 21:57:39 +0200

    fix fibonacci variables

--- hw1/fibonacci.rb
+++ hw1/fibonacci.rb
@@ -9,14 +9,14 @@ class Fibonacci
 
   def each
     i = 0
-    n0 = 0
-    n1 = 1
-    n = 1
+    prev = 0
+    next_step = 1
+    current = 1
     loop do
-      yield n
-      n = n0 + n1
-      n0 = n1
-      n1 = n
+      yield current
+      current = prev + next_step
+      prev = next_step
+      next_step = current
       i += 1
       break if i == size
     end
