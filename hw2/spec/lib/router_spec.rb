94568d66b6acf6d98fa87eedb2c6c4ad994a6d97
Paul Pavlovsky <fatpaher@gmail.com>
2016-11-14 22:50:47 +0200

    hw2

--- hw2/spec/lib/router_spec.rb
+++ hw2/spec/lib/router_spec.rb
@@ -3,32 +3,67 @@ RSpec.describe Router do
     Router.new do
       get '/test', ->(env) { [200, {}, ['get test']] }
       post '/test', ->(env) { [200, {}, ['post test']] }
+      get '/test/:message', ->(env) { [200, {}, ['test with params']] }
+      get '/post/:name', ->(env) do
+        [200,
+         {},
+         [env['params']['name'], 'post show page']]
+      end
 
-      ##
-      # TODO: router should match path by pattern like
-      # Pattern: /posts/:name
-      # Paths:
-      # /post/about_ruby
-      # /post/43
-      # Cover this with tests.
-      #
-      get '/post/:name', ->(env) { [200, {}, ['post show page']] }
+      get '/post/:name/comment/:id', ->(env) do
+        [200,
+         {},
+         [env['params']['name'], env['params']['id'], 'post comment show page']]
+      end
     end
   end
 
   context 'when request is GET' do
-    let(:env) { { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'GET'} }
-
     it 'matches request' do
+      env = { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'GET' }
       expect(subject.call(env)).to eq [200, {}, ['get test']]
     end
   end
 
   context 'when request is POST' do
-    let(:env) { { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'POST'} }
-
     it 'matches request' do
+      env = { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'POST' }
       expect(subject.call(env)).to eq [200, {}, ['post test']]
+    end
+  end
+
+  context 'when request with params' do
+    it 'matches request' do
+      env_with_params = {
+        'REQUEST_PATH' => '/test/123-abc',
+        'REQUEST_METHOD' => 'GET'
+      }
+      expect(subject.call(env_with_params)).to eq [200, {}, ['test with params']]
+    end
+
+    it 'adds params to env' do
+      env_with_params = {
+        'REQUEST_PATH' => '/post/about_ruby',
+        'REQUEST_METHOD' => 'GET'
+      }
+      expect(subject.call(env_with_params)).to eq [200, {}, ['about_ruby', 'post show page']]
+    end
+
+    it 'adds few params to env' do
+      env_with_params = {
+        'REQUEST_PATH' => '/post/13/comment/48',
+        'REQUEST_METHOD' => 'GET'
+      }
+      expect(subject.call(env_with_params)).to eq [200,
+                                                   {},
+                                                   ['13', '48', 'post comment show page']]
+    end
+  end
+
+  context 'when route not found' do
+    it 'returns 404 status when no page found' do
+      wrong_env = { 'REQUEST_PATH' => 'wrong_pass', 'REQUEST_METHOD' => 'GET' }
+      expect(subject.call(wrong_env)).to eq [404, {}, ['Page not found']]
     end
   end
 end
