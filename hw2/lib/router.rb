eebc0b767a5a5744b27760bad3306191aa3690da
Pavelgyravel <pavelgyravel@gmail.com>
2016-11-14 23:04:51 +0200

    Router completed

--- hw2/lib/router.rb
+++ hw2/lib/router.rb
@@ -1,6 +1,6 @@
 class Router
   def call(env)
-    @routes[env['REQUEST_METHOD']][env['REQUEST_PATH']].call(env)
+    select_route(env).call(env)
   end
 
 private
@@ -20,6 +20,22 @@ private
 
   def match(http_method, path, rack_app)
     @routes[http_method] ||= {}
-    @routes[http_method][path] = rack_app
+    @routes[http_method][regexp_method(path)] = {
+      :app => rack_app,
+      :pattern => path
+    }
   end
+
+  def regexp_method(path) 
+    Regexp.new(path.gsub(/:[a-zA-Z0-9_]+/, '[a-zA-Z0-9_]+'))
+  end
+
+  def select_route(env) 
+    @routes[env['REQUEST_METHOD']].each do |path_regexp, route|
+      if env['REQUEST_PATH'] =~ path_regexp
+        return route[:app]
+      end
+    end
+    return ->(env) {[404, {}, ['Page not found']]}
+  end 
 end
