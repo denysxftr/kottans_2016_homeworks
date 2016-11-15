49bed24b0d406a7b9151e754c315a880db2cdeb3
bsydorenko <b.sydorenko@gmail.com>
2016-11-15 17:18:08 +0200

    update

--- hw2/app/app.rb
+++ hw2/app/app.rb
@@ -1,6 +1,4 @@
 Application = Router.new do
   get '/test', ->(env) { [200, {}, ['get test']] }
   post '/test', ->(env) { [200, {}, ['post test']] }
-else
-  post '/', ->(env) { [404, {}, ['not found']] }
 end
