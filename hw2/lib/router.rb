0633d16756be43188b7ef379a71f9d9f424fac41
superedriver <superedriver@gmail.com>
2016-11-16 12:41:54 +0200

    Remade structure

--- hw2/lib/router.rb
+++ hw2/lib/router.rb
@@ -3,41 +3,26 @@
 "simple"  - to storage routes without ":" "/test/some_path"
 "complex"  - to storage routes with ":" "/test/:name
 
-@routes = {
-  "GET":
-    "simple": {
-      "/test": RACK_APP_1
-    }
-    "complex": {
-      "/test/:name": RACK_APP_3
-      "/test/:name/:name": RACK_APP_4
-    }
-  "POST": {
-    "simple": {
-      "/test": RACK_APP_2
-      "/test/some_path": RACK_APP_5
-    }
+@routes = [
+  {
+    pattern: path,
+    app: rack_app,
+    regexp: path_to_regexp(path)
   }
-}
+]
+
 =end
 
 class Router
   def call(env)
-    current_method, current_path  = env['REQUEST_METHOD'], env['REQUEST_PATH']
-    # simple routes handling
-    return routes[current_method]['simple'][current_path].call(env) if simple_path?(current_path, current_method)
-    # complex routes handling
-    path = complex_path?(current_path, current_method)
-    return routes[current_method]['complex'][path].call(env) if path.is_a? String
-    # 404 routes handling
-    ->(env) { [404, {}, ['not found']] }.call(env)
+    find_route(env).call(env)
   end
 
 private
   attr_reader :routes
 
   def initialize(&block)
-    @routes = {}
+    @routes = []
     instance_exec(&block)
   end
 
@@ -50,47 +35,20 @@ private
   end
 
   def match(http_method, path, rack_app)
-    routes[http_method] ||= {}
-    routes[http_method]['complex'] ||= {}
-    routes[http_method]['simple'] ||= {}
-
-    if path =~ /\/:\w/
-      routes[http_method]['complex'][path] = rack_app
-    else
-      routes[http_method]['simple'][path] = rack_app
-    end
+    routes << { pattern: path, app: rack_app, regexp: path_to_regexp(path), method: http_method}
   end
 
-  def simple_path?(current_path, current_method)
-    routes[current_method]['simple'].keys.each do |path|
-      return true if current_path == path
+  def find_route(env)
+    routes.each do |route|
+      if env['REQUEST_METHOD'] == route[:method] && env['REQUEST_PATH'] =~ route[:regexp]
+        return route[:app]
+      end
     end
 
-    false
+    ->(_env) { [404, {}, ['not found']] }
   end
 
-  def complex_path?(current_path, current_method)
-    routes[current_method]['complex'].keys.each do |path|
-      path_array, current_path_array = path.split('/'), current_path.split('/')
-      next if different_path_size?(path_array, current_path_array) || different_pathes?(path_array, current_path_array)
-
-      return path
-    end
-  end
-
-  def different_path_size?(path_array, current_path_array)
-    path_array.size != current_path_array.size
-  end
-
-  def different_pathes?(path_array, current_path_array)
-    path_array.each_with_index do |_,index|
-      return true unless same_items_of_path?(path_array[index], current_path_array[index])
-    end
-
-    false
-  end
-
-  def same_items_of_path?(path_item, current_path_item)
-    path_item == current_path_item || (path_item =~ /:\w/ && current_path_item =~ /\w/)
+  def path_to_regexp(path)
+    Regexp.new('\A' + path.gsub(/:[\w-]+/, '[\w-]+') + '\Z')
   end
 end
