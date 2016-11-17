bbe4499076e61ee49ff6960af1e2d8898121ed2e
Sergey Lysenko <lysenko.sergei.developer@gmail.com>
2016-11-17 12:48:02 +0200

    add router tests; re-write router to custom path

--- hw2/app/app.rb
+++ hw2/app/app.rb
@@ -1,4 +1,10 @@
 Application = Router.new do
   get '/test', ->(env) { [200, {}, ['get test']] }
   post '/test', ->(env) { [200, {}, ['post test']] }
+
+  get '/users/:id', ->(env) { [200, {}, ['post /users/:id']] }
+  get '/users/:id/comments/:comment/rating/:rating', ->(env) { [200, {}, ['post with users comment rating']] }
+  get '/users/articles/:title', ->(env) { [200, {}, ['post with user articles title']] }
+  # get '/', ->(env) { [404, {}, ['page not found']] }
+  # get '/test/:id/with/:smth', ->(env) { [200, {}, ['get test with id value']] }
 end
--- hw2/lib/router.rb
+++ hw2/lib/router.rb
@@ -1,25 +1,57 @@
 class Router
   def call(env)
-    @routes[env['REQUEST_METHOD']][env['REQUEST_PATH']].call(env)
+    find_route(env).call(env)
   end
 
 private
 
   def initialize(&block)
-    @routes = {}
+    @routes = []
     instance_exec(&block)
   end
 
+  def find_route(env)
+    # puts "call find_route #{@routes}"
+    @routes.each do |route|
+      # puts "#{env['REQUEST_METHOD']} ||||| #{route[:method]} ||||| #{env['REQUEST_PATH']} 
+      # |||| #{route[:regexp]}"
+      if env['REQUEST_METHOD'] == route[:method] && env['REQUEST_PATH'] =~ route[:regexp]
+        # puts "return true if REQUEST_METHOD == method"
+        env['router.params'] = extract_params(route[:pattern], env['REQUEST_PATH'])
+        return route[:app]
+      end
+    end
+
+    return ->(_env) { [404, {}, ['not found']] }
+  end
+
+
+  def path_to_regexp(path)
+    Regexp.new('\A' + path.gsub(/:[\w-]+/, '[\w-]+') + '\Z')
+  end
+
+  def extract_params(pattern, path)
+    pattern
+      .split('/')
+      .zip(path.split('/'))
+      .reject { |e| e.first == e.last }
+      .map { |e| [e.first[1..-1], e.last] }
+      .to_h
+  end
+
+  def match(http_method, path, rack_app)
+    # puts "call match"
+    @routes << { pattern: path, app: rack_app, regexp: path_to_regexp(path), method: http_method }
+  end
+
   def get(path, rack_app)
+    # puts "call get"
     match('GET', path, rack_app)
   end
 
   def post(path, rack_app)
+    # puts "call post"
     match('POST', path, rack_app)
   end
 
-  def match(http_method, path, rack_app)
-    @routes[http_method] ||= {}
-    @routes[http_method][path] = rack_app
-  end
 end
