865fbc4b47ed1233e29170258c9fe173271bcf87
Nik <nik.kholin@gmail.com>
2016-11-18 00:49:15 +0200

    create hmlons_web gem

--- hw2/spec/lib/router_spec.rb
+++ hw2/spec/lib/router_spec.rb
@@ -3,15 +3,6 @@ RSpec.describe Router do
     Router.new do
       get '/test', ->(env) { [200, {}, ['get test']] }
       post '/test', ->(env) { [200, {}, ['post test']] }
-
-      ##
-      # TODO: router should match path by pattern like
-      # Pattern: /posts/:name
-      # Paths:
-      # /post/about_ruby
-      # /post/43
-      # Cover this with tests.
-      #
       get '/post/:name', ->(env) { [200, {}, ['post show page']] }
     end
   end
