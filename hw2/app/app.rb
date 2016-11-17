26fe230d66449c9eb96ed117bde16e2774f1859b
Andrii Melnyk <gipson.ukr@gmail.com>
2016-11-17 20:22:08 +0200

    * add tests for requests with params * refactoring rspec

--- hw2/app/app.rb
+++ hw2/app/app.rb
@@ -1,4 +1,8 @@
 Application = Router.new do
   get '/test', ->(env) { [200, {}, ['get test']] }
   post '/test', ->(env) { [200, {}, ['post test']] }
+  get '/post/:name', ->(env) { [200, {}, ['post show page']] }
+  get '/blog/:page_id/posts/:post_id', lambda do |env|
+    [200, {}, ["get blog page: #{env[request_symbols][page_id]} post: #{env[request_symbols][post_id]}"]]
+  end
 end
--- hw2/spec/lib/router_spec.rb
+++ hw2/spec/lib/router_spec.rb
@@ -3,29 +3,40 @@ RSpec.describe Router do
     Router.new do
       get '/test', ->(env) { [200, {}, ['get test']] }
       post '/test', ->(env) { [200, {}, ['post test']] }
-
-      ##
-      # TODO: router should match path by pattern like
-      # Pattern: /posts/:name
-      # Paths:
-      # /post/about_ruby
-      # /post/43
-      # Cover this with tests.
-      #
       get '/post/:name', ->(env) { [200, {}, ['post show page']] }
+      get '/blog/:page_id/posts/:post_id', (lambda do |env|
+        [200, {}, ["get blog page: #{env[request_symbols][page_id]} post: #{env[request_symbols][post_id]}"]]
+      end)
     end
   end
 
   context 'when request is GET' do
-    let(:env) { { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'GET'} }
+    let(:env) { { 'REQUEST_PATH' => '/test', 'REQUEST_METHOD' => 'GET' } }
 
-    it 'returns 404 for non existen pages' do
-      expect(subject.call('REQUEST_PATH' => '/not_exist', 'REQUEST_METHOD' => 'GET')).to eq [404, {}, ['Page not found']]
+    context 'when request is GET to non existen page' do
+      let(:env) { { 'REQUEST_PATH' => '/not_exist', 'REQUEST_METHOD' => 'GET' } }
+
+      it 'returns 404 for non existen pages' do
+        expect(subject.call(env)).to eq [404, {}, ['Page not found']]
+      end
     end
 
     it 'matches request' do
       expect(subject.call(env)).to eq [200, {}, ['get test']]
     end
+
+    context 'when request is GET with params' do
+      let(:env) { { 'REQUEST_PATH' => '/post/about_ruby', 'REQUEST_METHOD' => 'GET' } }
+
+      it 'matches request with params' do
+        expect(subject.call(env)).to eq [200, {}, ['post show page']]
+      end
+    end
+
+    # it 'matches request with params and substitutes them' do
+    #   env['REQUEST_PATH'] = '/blog/vasya/posts/about_ruby'
+    #   expect(subject.call(env)).to eq [200, {}, ['get blog page: vasya post: about_ruby']]
+    # end
   end
 
   context 'when request is POST' do
