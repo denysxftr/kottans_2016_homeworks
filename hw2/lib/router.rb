40348c99fb9b252e15f38575ef1dc75c97b67ea3
Alexander Maksimyak <alexander.max@gmail.com>
2016-11-17 12:00:50 +0200

    hw2 0.2.1

--- hw2/lib/router.rb
+++ hw2/lib/router.rb
@@ -1,7 +1,7 @@
 class Router
   def call(env)
-    @routes[env['REQUEST_METHOD']].each do |path_pattern, rack_app|
-      return rack_app.call(env) if path_pattern.match(env['REQUEST_PATH'])
+    @routes.each do |route|
+      return route[:app].call(env) if route[:http_method] == env['REQUEST_METHOD'] && route[:pattern].match(env['REQUEST_PATH'])
     end
     [404, {}, ['Not Found']]
   end
@@ -9,7 +9,7 @@ class Router
 private
 
   def initialize(&block)
-    @routes = {}
+    @routes = []
     instance_exec(&block)
   end
 
@@ -22,11 +22,11 @@ private
   end
 
   def match(http_method, path, rack_app)
-    @routes[http_method] ||= {}
-    @routes[http_method][pattern(path)] = rack_app
+    @routes ||= []
+    @routes << { http_method: http_method, app: rack_app, path: path, pattern: pattern(path) }
   end
 
   def pattern(path)
-    Regexp.new("^#{path.gsub(/:\w+/, '\w+')}\/?$")
+    Regexp.new("^#{path.gsub(/:[\w-]+/, '[\w-]+')}\/?$")
   end
 end
