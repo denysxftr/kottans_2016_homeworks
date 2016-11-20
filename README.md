a08172ee9e7c5f7c2fc0917a7d04ad972ff7873b
AlexPurhalo <alexpurhalo@gmail.com>
2016-11-20 19:59:25 +0200

    gem-initialization-II

---
+++ README.md
@@ -0,0 +1,6 @@
+<h3>Homework 3</h3>
+
+<h4>Usage</h4>
+<ul>
+    <li>cd hw3/asdetmin && bundle</li>
+</ul>
--- hw3/asdetmin/lib/asdetmin.rb
+++ hw3/asdetmin/lib/asdetmin.rb
@@ -1,5 +1,9 @@
 require "asdetmin/version"
 
 module Asdetmin
-  # Your code goes here...
+  class Router
+    def call(env)
+      [200, { "Content-Type" => "text/html" }, ["Hello"]]
+    end
+  end
 end
