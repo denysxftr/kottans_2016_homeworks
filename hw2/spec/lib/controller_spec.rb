e3fb174cdc3932c19f2a00dcf00293073a72ca6e
bsydorenko <b.sydorenko@gmail.com>
2016-11-21 12:49:20 +0200

    ..,..

--- hw2/spec/lib/controller_spec.rb
+++ hw2/spec/lib/controller_spec.rb
@@ -1,32 +1,32 @@
 RSpec.describe Controller do
-  subject do
-    Controller.new do
-      text: ['text/plain', ->(c) { c.to_s }],
-      json: ['application/json', ->(c) { Oj.dump(c) }]
-      end
+    subject do
+        Controller.new do
+            text ['text/plain', ->(c) { c.to_s }]
+            json ['application/json', ->(c) { Oj.dump(c) }]
+        end
     end
-  end
+end
 
-  context 'when json response' do
-    c = ('application/json')
+context 'when json response' do
+    c = 'application/json'
 
-  it 'successfully responds' do
-    c =  Oj.dump(c)
+    it 'successfully responds' do
+        c = Oj.dump(c)
     end
-  end
+end
 
-  context 'when text response' do
-      c = ('text/plain')
+context 'when text response' do
+    c = 'text/plain'
 
-  it 'successfully responds' do
-    c = c.to_s
+    it 'successfully responds' do
+        c = c.to_s
     end
-  end
+end
 
-  context 'when has router params' do
+context 'when has router params' do
     request.params
 
-  it 'successfully responds' do
-    @request.params.merge!(env['router.params'] || {})
+    it 'successfully responds' do
+        @request.params.merge!(env['router.params'] || {})
     end
-  end
+end
