edc777a4381d5fba6983cd2d7d84b1963c754516
xwzsd <shamik2024@gmail.com>
2016-11-25 00:59:02 +0200

    travis

--- check.sh
+++ check.sh
@@ -1,1 +1,1 @@
-ruby ./hw1/fibonacci_test.rb && (cd hw2 && bundle install && bundle exec rspec) && (cd hw3/hola && bundle install && bundle exec rspec)
+ruby ./hw1/fibonacci_test.rb && (cd hw2 && bundle install && bundle exec rspec)
