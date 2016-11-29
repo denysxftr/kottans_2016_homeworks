cb512c551cee78da4983210729cd979369a816d0
Andrii Melnyk <gipson.ukr@gmail.com>
2016-11-29 17:35:38 +0200

    added tests

--- hw4/spec/app/app_spec.rb
+++ hw4/spec/app/app_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe 'hw4 app' do
     get '/test'
     expect(last_response.status).to be 200
   end
+
+  it 'test page should return text' do
+    get '/test'
+    expect(last_response.headers['Content-Type']).to eq 'text/plain'
+  end
+
+  it 'test page should return request method' do
+    get '/test'
+    expect(last_response.body).to eq 'Request method: GET'
+  end
 end
