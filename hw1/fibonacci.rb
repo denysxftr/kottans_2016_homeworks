0c3bbb509f736da5030a158856f37086a3bf7905
Vitalii Onufryk <vitaliy.onufryk@pb.com>
2016-12-06 11:49:50 +0200

    Updated Fibonacci hw1

--- hw1/fibonacci.rb
+++ hw1/fibonacci.rb
@@ -7,16 +7,13 @@ class Fibonacci
   end
 
   def each
-    fib_prev = 1
-    fib_cur = 1
-    @num.times do |i|
-      if i == 1
-        yield 1
-      else
-        yield fib_cur
-        fib_cur, fib_prev = (fib_cur + fib_prev), fib_cur
-      end
+    fib_prev = 0
+    fib_curr = 1
+    @num.times do
+      yield fib_curr
+      temp = fib_curr
+      fib_curr = fib_curr + fib_prev
+      fib_prev = temp
     end
   end
-
 end
